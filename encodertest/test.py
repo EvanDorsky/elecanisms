0b10d469c6cb22b99cf16bd5708a115682f2a704
Evan Dorsky <evan.dorsky@icloud.com>
2016-02-02 21:28:21 -0500

    Make test roll

--- encodertest/test.py
+++ encodertest/test.py
@@ -5,10 +5,11 @@ enc = encodertest.encodertest()
 
 enc.toggle_led3()
 
-# while (True):
-angleBytes = enc.enc_readReg(enc.ENC_ANGLE_AFTER_ZERO_POS_ADDER)
+while (True):
+    angleBytes = enc.enc_readReg(enc.ENC_ANGLE_AFTER_ZERO_POS_ADDER)
 
-mask = 0x3FFF
-angle = int(angleBytes[0])+int(angleBytes[1])*256
-time.sleep(1e-1)
-print "Bin: {0:016b} Hex:{0:04x}".format(angle, angle)
+    mask = 0x3FFF
+    angle = int(angleBytes[0])+int(angleBytes[1])*256
+    print "Bin: {0:016b} Hex:{1:04x} Dec:{2:0f}".format(angle, angle, float(angle&mask)/mask*360)
+
+    time.sleep(.02)
