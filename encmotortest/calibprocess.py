f81905d3436d282f0a66ce2fc07793367deab279
Evan Dorsky <evan.dorsky@icloud.com>
2016-02-07 01:51:13 -0500

    Generate nice calibration plots!

--- encmotortest/calibprocess.py
+++ encmotortest/calibprocess.py
@@ -19,9 +19,30 @@ encpos = -wrapfix(encpos)
 encpos -= encpos[int(len(encpos)/2)]
 angles -= angles[int(len(encpos)/2)]
 
-plt.scatter(angles, encpos)
+[m1, b1] = polyfit(angles, encpos, 1)
+
+estangles = encpos/(15+1/3)
+
+[m2, b2] = polyfit(angles, estangles, 1)
+
 plt.grid(True)
-plt.title('Calibration Curve')
+plt.scatter(angles, encpos)
+plt.hold(True)
+plt.plot(angles, m1*angles+b1)
+plt.title('Calibration Curve 1')
 plt.xlabel('Joystick Angle (deg)')
 plt.ylabel('Encoder Angle (deg)')
+plt.ylim([-800, 800])
+plt.legend(['Best Fit', 'Data'], loc='upper left')
+
+plt.figure()
+plt.grid(True)
+plt.scatter(angles, estangles)
+plt.hold(True)
+plt.plot(angles, m2*angles+b2)
+plt.title('Calibration Curve 2')
+plt.xlabel('Joystick Angle (deg)')
+plt.ylabel('Corrected Encoder Angle (Based on CAD)')
+plt.legend(['Best Fit', 'Data'], loc='upper left')
+
 plt.show()
