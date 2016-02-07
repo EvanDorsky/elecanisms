85a7f4825b04c78dac1b8594020bf78a7da817e8
Evan Dorsky <evan.dorsky@icloud.com>
2016-02-06 22:29:41 -0500

    Calibration script

--- encmotortest/calibrate.py
+++ encmotortest/calibrate.py
@@ -6,46 +6,58 @@ import pickle
 
 mask = 0x3FFF
 enc = encodertest.encodertest()
-
 enc.toggle_led3()
 
-samples = 2000
-encpos = zeros(samples)
-times = zeros(samples)
+def getAngle():
+    angleBytes = enc.enc_readReg(enc.ENC_ANGLE_AFTER_ZERO_POS_ADDER)
+    raw = int(angleBytes[0])+int(angleBytes[1])*256
+
+    return float(raw&mask)/mask*360
+
+def printSample(i):
+    if i:
+        print ""
+        print "Another one."
+        print ""
+    print "Sample "+str(i)
+    print "YOU HAVE "+str(5)+" SECONDS"
+
+ticks = 19
+window = 50
+encpos = zeros(ticks)
+times = zeros(ticks)
 raw = 0
 
 last = time.clock()
-step = 1e-3
+step = 5
 i = 0
 
-while(i < samples):
+encsum = 0
+
+angles = linspace(0, 90, ticks)
+
+print "Get ready!"
+print "YOU HAVE "+str(5)+" SECONDS"
+
+while(i < ticks):
     current = time.clock()
 
     if (current - last > step):
-        times[i] = current
+        printSample(i)
+        for samp in range(0, window):
+            encsum += getAngle()
+
+        encpos[i] = encsum/window
+
+        encsum = 0
         last = current
-
-        angleBytes = enc.enc_readReg(enc.ENC_ANGLE_AFTER_ZERO_POS_ADDER)
-        raw = int(angleBytes[0])+int(angleBytes[1])*256
-        encpos[i] = float(raw&mask)/mask*360
-
         i+=1
 
-encdiff = append(array(0), diff(encpos))
-
-possteps = cumsum(-360*(encdiff>180))
-negsteps = cumsum(360*(encdiff<-180))
-
-encpos += possteps+negsteps
-encvel = divide(diff(encpos), diff(times))
-
 pickle.dump({
-    'position' : encpos,
-    'velocity' : encvel,
-    'time'     : times
-    }, open('spindown.p', 'wb'))
+    'position' : encpos
+    }, open('calibration.p', 'wb'))
 
-plt.plot(times, encpos)
+plt.plot(encpos)
 plt.hold(True)
 plt.plot(times[0:-1], encvel)
 plt.show()
